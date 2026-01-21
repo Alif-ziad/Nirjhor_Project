eb2a43bf5404ae3b6f53b96febc06c77e9289a7b
Alif-ziad <ziadalif01@gmail.com>
2026-01-21 16:21:53 +0600

    everything is functional

--- nirjhor.sql
+++ nirjhor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2026 at 01:44 PM
+-- Generation Time: Jan 21, 2026 at 11:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,6 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `address`, `total_amount`, `created_at`) VALUES
+(1, 1, 'here', 72.00, '2026-01-21 10:09:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_name`, `price`, `quantity`) VALUES
+(1, 1, 'dana', 11.00, 2),
+(2, 1, 'longcoat', 50.00, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -34,16 +77,18 @@ CREATE TABLE `products` (
   `description` text NOT NULL,
   `seller_id` int(11) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `status` enum('pending','approved','denied') NOT NULL DEFAULT 'pending'
+  `status` enum('pending','approved','denied') NOT NULL DEFAULT 'pending',
+  `is_approved` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `name`, `price`, `description`, `seller_id`, `image`, `status`) VALUES
-(13, 'longcoat', 50.00, 'dfgdftg', 3, '../assets/images/1768734440_Screenshot (409).png', 'approved'),
-(14, '###', 10.00, 'cfsdrfgrs', 3, '../assets/images/1768734760_Screenshot (153).png', 'approved');
+INSERT INTO `products` (`product_id`, `name`, `price`, `description`, `seller_id`, `image`, `status`, `is_approved`) VALUES
+(13, 'longcoat', 50.00, 'dfgdftg', 3, '../assets/images/1768734440_Screenshot (409).png', 'approved', 0),
+(14, '###', 10.00, 'cfsdrfgrs', 3, '../assets/images/1768734760_Screenshot (153).png', 'approved', 0),
+(16, 'dana', 11.00, 'sads', 6, '../assets/images/1768990048_anime-cyberpunk-24-09-2024-1727169479-hd-wallpaper (2).jpg', 'approved', 0);
 
 -- --------------------------------------------------------
 
@@ -57,21 +102,38 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','seller','customer') NOT NULL,
-  `status` enum('pending','approved','blocked') NOT NULL
+  `status` enum('pending','approved','blocked') NOT NULL,
+  `shop_name` varchar(100) DEFAULT NULL,
+  `shop_address` varchar(255) DEFAULT NULL,
+  `is_approved` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `status`) VALUES
-(1, 'toji', 'toji2011@gmail.com', 'toji', 'customer', 'approved'),
-(2, 'nezo', 'nezo1999@gmail.com', 'nezo', 'admin', 'approved'),
-(3, 'fumi', 'fumi2000@gmail.com', 'fumi', 'seller', 'approved');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `status`, `shop_name`, `shop_address`, `is_approved`) VALUES
+(1, 'toji', 'toji2011@gmail.com', 'toji', 'customer', 'approved', NULL, NULL, 1),
+(2, 'nezo', 'nezo1999@gmail.com', 'nezo', 'admin', 'approved', NULL, NULL, 1),
+(3, 'fumi', 'fumi2000@gmail.com', 'fumi', 'seller', 'approved', NULL, NULL, 1),
+(4, 'minho', 'minho@gmail.com', '1234', 'customer', 'pending', '', '', 1),
+(6, 'tamashi', 'tamashi@gmail.com', '1234', 'seller', 'approved', 'fgbgfb', 'fgbgfb', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -91,16 +153,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
